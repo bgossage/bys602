ae5041eccc570f31d4d1ec90bac29cca6101115d
bgossage <bgossagen@gmail.com>
2017-01-24 18:04:07 -0600

    Moved parse implementation to genebank module

--- Assignment_1/Brett_Gossage.py
+++ Assignment_1/Brett_Gossage.py
@@ -11,118 +11,31 @@
 
 import sys
 
+#
+# Setup path to genebank module...
+#
+sys.path.append( "../modules" )
+
+import genebank
 
 filename = "S_invicta_sequence.gb"
 
-keywords = {
-              "DEFINITION", "AUTHORS", "ACCESSION",
-              "TITLE", "REFERENCE", "FEATURES", "ORIGIN",
-              "LOCUS", "JOURNAL", "ORGANISM", "VERSION",
-              "KEYWORDS", "source", "gene", "CDS", 
-              "sig_peptide", "mat_peptide" 
-              "//"
-           }
-
-filtered = { "TITLE", "DEFINITION", "AUTHORS", "ACCESSION", "CDS" }
-
-def handle_definition( record, output=sys.stdout ):
-   
-   loc = record.rfind( "DEFINITION" )
-   
-   data = record[loc+12:]
-
-   output.write( "Definition: {:s}".format( data ) )
-      
-#end handle_definition() ~~~~~~~~~~~~~~~~~~~~~~~~
-
-handlers = { "DEFINITION" : handle_definition }
-
-reject = keywords - filtered
-
-print reject
 
 if( len(sys.argv) > 1 ):
    filename = sys.argv[1] 
    
-reading_record = False
-key = ""
-read_key = ""
-content = ""
+parser = genebank.GenBankParser()
 
-#
-# Open the input file and cleanly handle exceptions.
-#
-with open( filename, "r" ) as seq_file:
+filter_keys = { "DEFINITION" }
 
-#
-# Open the output file and cleanly handle exceptions., "\n"
-#
-   with open( "seq_parse.txt", "w" ) as output_file:
+parser.set_filtered( filter_keys )
 
-      line_num = 0
-      
-      for line in seq_file:
-         
-         if line.startswith( "LOCUS" ):
-            print "======== START RECORD ============================"
-            output_file.write( "======== START RECORD =============================\n" )
+with open( "seq_parse.txt", "w" ) as output_file:
 
-         tokens = line.split()
+     parser.parse( filename, output_file )
+     
+#end with output_file
+   
 
-         if not tokens:
-            continue
-         
-         key = tokens[0]
-         
-      # If we are reading a record for a given key...
-         if reading_record :
-         # And we encounter any keyword..
-            if key in keywords:
-            # We've reached the end of the record...
-               reading_record = False
-
-            # Process the record content...
-#            print ",",key,","
-               if read_key in handlers:
-                  handlers[read_key]( content, output_file )
-            
-            #   print content
-               #output_file.write( content )
-
-            # Clear the content string...
-               content = ""
-            #end if key
-         # end if reading
-         
-         # Otherwise, add the current line to the record content...
-            else:
-               content += line
-
-         #end if
-    
-      # If we are not reading a record...
-         if not reading_record:
-
-         # And if we encounter a key to be read...
-            if key in filtered:
-            # We've reached the beginning of a record... 
-               reading_record = True
-               read_key = key
-            # Add the current line to the record content...
-               content += line
-         #end if
-         
-      # If we encounter the end of a locus,
-         if key == "//":
-         # Print an empty line...
-            print "======== END RECORD =============================\n"
-            output_file.write( "======== END RECORD =============================\n\n" )
-         #end if
-
-      # end for line
-
-   # end with outputfile
-
-# end with seq_file
 
 # EOF
