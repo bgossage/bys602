c4d2d19487caa70ae0ef098bf6ade3a057c15944
Brett Gossage <bgossage3@comcast.net>
2017-01-28 11:41:48 -0600

    Added documentation

--- Assignment_1/Brett_Gossage.py
+++ Assignment_1/Brett_Gossage.py
@@ -5,7 +5,7 @@
 #
 # Brett Gossage (bgossgen@gmail.com)
 #
-# Assignment 1, Jan 14, 2017
+# Assignment 1, Jan 27, 2017
 #
 
 
@@ -20,26 +20,32 @@ import genebank
 
 filename = "S_invicta_sequence.gb"
 
-
+# If provided, use the filename from the command line arg...
 if( len(sys.argv) > 1 ):
    filename = sys.argv[1] 
    
+# Create a GenBank file parser...
 parser = genebank.GenBankParser()
 
+# Define the set of keywords we are going to process...
 filter_keys = { "DEFINITION", "ACCESSION", "AUTHORS", "CDS", "gene" }
 
 parser.set_filtered( filter_keys )
 
+# Create a list of gene locus data records to be filled by the parser...
 loci = []
 
+# Open the output file and assure is gets closed...
 with open( "seq_parse.txt", "w" ) as output_file:
 
+# Parse the given GenBank file...
    parser.parse( filename, loci )
 
+# Print out the list of locus records...
    for locus in loci:
-      
+   # Print the locus to the screen...
       print locus
-      
+   # Also write the request data to the output file...
       output_file.write( "\n\nLOCUS: \n" )
       
       output_file.write( "Definition: {:s}\n".format( locus.definition ) )
@@ -51,8 +57,6 @@ with open( "seq_parse.txt", "w" ) as output_file:
       output_file.write( "///\n" )
       
 #end with output_file
-
-   
 
 
 # EOF
