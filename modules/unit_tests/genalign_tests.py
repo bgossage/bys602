8bbac2f6eb946d31e5b92376539bc973ea9d9277
Brett Gossage <bgossage3@comcast.net>
2017-02-09 07:05:59 -0600

    New file

---
+++ modules/unit_tests/genalign_tests.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+"""
+GeneBank module unit tests.
+
+Each class/method is tested against expected results from the given inputs.
+
+(Uses the unit test module from the python libraries (similar to JUnit.)
+
+"""
+
+import sys
+
+sys.path.append( ".." )
+
+
+import unittest
+import genalign
+import numpy
+import math
+
+class GeneAlign_tests( unittest.TestCase ):
+
+   """ 
+      .
+   """
+   def test_sequence_align( self ):
+              
+      seq1 = "ARND"  
+      seq2 = "ARNE"   
+              
+      substMatrix = genalign.SubstitutionMatrix()
+      
+      
+      
+      scoreMatrix = genalign.ScoringMatrix( substMatrix, seq1, seq2 )
+      
+      print scoreMatrix.score_matrix
+      print scoreMatrix.arrow
+
+     # self.assertEqual( loci[0].definition, "Solenopsis invicta venom allergen 3 (LOC105199703), mRNA." )
+      
+   #end test_parse ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# end class GeneBank_tests
+
+if __name__ == '__main__':
+    unittest.main()
+
+# EOF
