559cda10eac81d4300693a59ef20fae656c4b123
Brett Gossage <bgossage@linux.hntsvlle1.al.home.com>
2017-02-14 19:17:16 -0600

    Initial working version

--- modules/unit_tests/genalign_tests.py
+++ modules/unit_tests/genalign_tests.py
@@ -28,17 +28,23 @@ class GeneAlign_tests( unittest.TestCase ):
               
       seq1 = "ARND"  
       seq2 = "ARNE"   
+      
+      match = 1.0
+      not_match = -1.0
               
-      substMatrix = genalign.SubstitutionMatrix()
-      
-      
+      substMatrix = genalign.SubstitutionMatrix(match,not_match)
       
       scoreMatrix = genalign.ScoringMatrix( substMatrix, seq1, seq2 )
-      
-      scoreMatrix.backtrace( seq1, seq2 )
-      
+
       print scoreMatrix.score_matrix
       print scoreMatrix.arrow
+           
+      scoreMatrix.backtrace( seq1, seq2 )
+      
+      aligned_seq1, aligned_seq2 = scoreMatrix.backtrace( seq1, seq2 )
+      
+      print aligned_seq1
+      print aligned_seq2
 
      # self.assertEqual( loci[0].definition, "Solenopsis invicta venom allergen 3 (LOC105199703), mRNA." )
       
